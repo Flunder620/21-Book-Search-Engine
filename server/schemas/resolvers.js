e45ee9f832c0541306b8c6125e8fada2e0c7df29
Flunder620 <jguzman12310@yahoo.com>
2024-01-13 12:59:36 -0600

    worked on type defs and resolvers :

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,59 @@
+// import user model
+const { User } = require("../models");
+// import sign token function from auth
+const { signToken, AuthenticationError } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    getSingleUser: async (parent, args, context) => {
+      const foundUser = await User.findOne({ _id: context.user._id });
+
+      if (!foundUser) {
+        throw new AuthenticationError("User not found");
+      }
+
+      return foundUser;
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, args, context) => {
+      const user = await User.create(args);
+      if (!user) {
+        throw new AuthenticationError("Can't create user");
+      }
+
+      return { token, user };
+    },
+    login: async (parent, args, context) => {
+      const user = await User.findOne({ email: args.email });
+
+      if (!user) {
+        throw new AuthenticationError("User not found");
+      }
+
+      const correctPw = await user.isCorrectPassword(args.password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong Password");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent,args,context) => {
+        try{
+            const updatedUser= await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: args } },
+                { new: true, runValidators: true }
+            )
+        } catch (err) {
+            throw new AuthenticationError;
+        }
+    },
+    deleteBook: async (parent,args,context) => {
+
+    }
+  },
+};
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,34 @@
+const typeDefs = `
+    type User{
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+    type Book {
+        _id: ID
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth{
+        token: ID
+        user: User
+    }
+    
+    type Query{
+        getSingleUser: User
+
+    }
+
+    type Mutation{
+        createUser(username: String, email: String, password: String): Auth
+        login(username: String, email: String, password: String): Auth
+    }
+`
