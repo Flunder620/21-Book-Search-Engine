2280b8ff7cbd725800164db5245514379dc2a4c2
Flunder620 <jguzman12310@yahoo.com>
2024-01-14 14:27:26 -0600

    added add user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,16 @@ mutation  loginUser($email: String, $password: String){
     }
   }
 `
+
+export const ADD_USER = gql `
+mutation addUser($username: String, $email: String, $password: String) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        email
+        password
+        username
+      }
+    }
+  }
+`
